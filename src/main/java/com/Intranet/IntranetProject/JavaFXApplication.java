3a5f05617eced1ff01726fd368fe0137c428e35b
nielsperdon <niels.perdon@gmail.com>
2022-12-13 12:38:27 +0100

    Initial commit

---
+++ src/main/java/com/Intranet/IntranetProject/JavaFXApplication.java
@@ -0,0 +1,12 @@
+package com.Intranet.IntranetProject;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFXApplication {
+
+    public static void main(String[] args) {
+        Application.launch(IntranetProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/Intranet/IntranetProject/IntranetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Intranet.IntranetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
